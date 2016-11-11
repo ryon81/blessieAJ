f9dcc95aebc9fa3736b23597f4ee7e9989c821fa
ryon81 <ryon81@naver.com>
2016-11-11 10:50:04 +0900

    Add files via upload
    first upload

---
+++ ActionPoint.java
@@ -0,0 +1,48 @@
+package summonerswar_test;
+
+import java.util.*;
+
+public class ActionPoint extends Monsters implements Apable
+{				
+	
+	public void checkAP()
+	{
+		/*try
+		{*/	
+		while(true)
+		{			
+			System.out.print("AP는? ");
+			Scanner s = new Scanner(System.in);
+			int tmp = s.nextInt();
+			System.out.println("전체 행동력이 " + tmp + "증가 되었습니다.");
+			super.actionPoint += tmp;						
+			apFill(super.actionPoint);
+			s.close();
+		}
+									
+		/*} catch (Exception ex)
+		{
+			System.out.print("프로그램 비정상 종료");
+			System.exit(-1);
+		}*/		
+	}		
+	
+	public void apFill(int actionPoint)
+	{					
+		System.out.println("전체 행동력: " + actionPoint);
+		new Attacker().checkAttacker(actionPoint);
+		new Defender().checkAttacker(actionPoint);
+		new Supporter().checkAttacker(actionPoint);
+		new Physical().checkAttacker(actionPoint);
+	} 
+	
+	
+	
+	void checkBossAp(){};
+	
+	void checkDefenderAp(){};
+		
+	void checkPhysicalAp(){};
+	
+	void checkSupporterAp(){};
+}
