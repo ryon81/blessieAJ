4ca64e2b410f639bfd9374e938d1534a541c26bd
ryon81 <ryon81@naver.com>
2016-11-17 11:58:21 +0900

    Add new class work at 2016-11-17

---
+++ Chapter15/src/IOEx02.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class IOEx02 
+{
+	public static void main(String[] args) 
+	{
+		byte[] inSrc ={0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+		byte[] outSrc = null;
+		
+		byte[] temp = new byte[10];
+				
+		ByteArrayInputStream input = null;
+		ByteArrayOutputStream output = null;
+		
+		input = new ByteArrayInputStream(inSrc);
+		output = new ByteArrayOutputStream();
+				
+		input.read(temp, 0, temp.length);
+		output.write(temp, 5, 5);
+		
+		
+		outSrc = output.toByteArray();
+		
+		System.out.println("Input Source  :" + Arrays.toString(inSrc));
+		System.out.println("Temp          :" + Arrays.toString(temp));
+		System.out.println("Output Source :" + Arrays.toString(outSrc));
+
+	}
+
+}
