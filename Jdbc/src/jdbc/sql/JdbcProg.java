5bf0614b962f0f84699222b75c7f8b0c760b72d9
ryon81 <ryon81@naver.com>
2016-11-22 00:07:10 +0900

    Update JdbcProg.java
    updated on 16/11/22

--- Jdbc/src/jdbc/sql/JdbcProg.java
+++ Jdbc/src/jdbc/sql/JdbcProg.java
@@ -12,8 +12,36 @@ public class JdbcProg
 		int empNo;
 		String ename;
 		
+		try
+		{
+			Class.forName("oracle.jdbc.driver.OracleDriver");			
+		} catch(ClassNotFoundException e)
+		{
+			System.err.println("ClassNotFoundException: " + e.getMessage());
+		}
 		
-
+		try 
+		{
+			con = DriverManager.getConnection(jdbc_url, "scott", "tiger");
+			stmt = con.createStatement();
+			String sql = "SELECT * FROM emp";
+			ResultSet rs = stmt.executeQuery(sql);
+			System.out.println("empno" + " " + "ename");
+			System.out.println("=====================");
+			
+			while (rs.next())
+			{
+				empNo = rs.getInt("empno");
+				ename = rs.getString("ename");
+				System.out.println(empNo + " " + ename);
+			}
+			
+			rs.close();
+			stmt.close();
+			con.close();					
+		} catch (SQLException e)
+		{
+			System.err.println("SQLException: " + e.getMessage());
+		}
 	}
-
 }
