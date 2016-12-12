961957c9417f60dc0ec06e0ff6b71e44f5c87ee6
ryon81 <ryon81@naver.com>
2016-12-13 00:43:59 +0900

    add updated homework 16/12/13

---
+++ Final/src/GUI/MainDisplay.java
@@ -0,0 +1,85 @@
+package GUI;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.Graphics;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+
+class MainDisplay extends JFrame
+{	
+	Container contentPane;
+	
+	MainDisplay()
+	{
+		setTitle("나만의 가계부");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		contentPane = getContentPane();
+		JTabbedPane pane = createTabbedPane();
+		contentPane.add(pane, BorderLayout.CENTER);
+				
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		JMenu mnFile = new JMenu("File");
+		menuBar.add(mnFile);
+		
+		JMenuItem mntmSave = new JMenuItem("Save");
+		mnFile.add(mntmSave);
+		
+		JMenuItem mntmLoad = new JMenuItem("Load");
+		mnFile.add(mntmLoad);
+		
+		JMenuItem mntmExit = new JMenuItem("Exit");
+		mnFile.add(mntmExit);
+		
+		JMenu mnUser = new JMenu("User");
+		menuBar.add(mnUser);
+		
+		JMenuItem mntmNew = new JMenuItem("New..");
+		mnUser.add(mntmNew);
+		
+		JMenuItem mntmLogout = new JMenuItem("Logout");
+		mnUser.add(mntmLogout);
+		
+		setSize(432,768);
+		setVisible(true);
+	}
+		
+	
+	JTabbedPane createTabbedPane()
+	{
+		JTabbedPane pane = new JTabbedPane();
+		pane.addTab("Calendar", new JLabel(new ImageIcon("img/calendar.png")));
+		pane.addTab("Report", new JLabel(new ImageIcon("img/report.png")));
+		pane.addTab("Money", new JLabel(new ImageIcon("img/money.png")));
+		pane.addTab("Budget", new JLabel(new ImageIcon("img/budget.png")));
+		return pane;
+	}
+	
+	class MyPanel extends JPanel
+	{
+		MyPanel() 
+		{
+			this.setBackground(Color.YELLOW);
+		}
+		public void paintComponent(Graphics g)
+		{
+			super.paintComponent(g);
+			g.setColor(Color.RED);
+		}
+	}
+	
+	public static void main(String[] args) 
+	{
+		new MainDisplay();
+	}
+}
--- Final/src/GUI/Register.java
+++ Final/src/GUI/Register.java
@@ -1,17 +1,16 @@
 package GUI;
 
 import java.awt.FlowLayout;
-import java.awt.Frame;
 import java.awt.Label;
 import java.awt.TextField;
 
 import javax.swing.JFrame;
 
-public class Login extends JFrame
+public class Register extends JFrame
 {
 	JFrame f = new JFrame();
 	
-	Login()
+	Register()
 	{		
 		f.setSize(400,100);
 		f.setLayout(new FlowLayout()); 
@@ -19,16 +18,21 @@ public class Login extends JFrame
 				
 		Label lid = new Label("ID: ", Label.RIGHT);
 		Label lpwd = new Label(("Password: "), Label.RIGHT);		
+		Label relpwd = new Label(("RE-Password: "), Label.RIGHT);
 		
 		TextField id = new TextField(10);
 		TextField pwd = new TextField(10);
+		TextField repwd = new TextField(10);
 		pwd.setEchoChar('*');  // <-- 비밀번호등 보안상으로 실제 캐릭터를 안 보이게 할 때 사용
-				
+		repwd.setEchoChar('*');
+		
 		f.add(lid);
 		f.add(id);
 		f.add(lpwd);
 		f.add(pwd);
+		f.add(relpwd);
+		f.add(repwd);
+		
 		f.setVisible(true);
 	}
-
 }
