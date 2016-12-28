e549715fb68c9526c1f795c6ddf4e8984c405e1d
ryon81 <ryon81@naver.com>
2016-12-28 11:55:59 +0900

    member테이블 수정

--- JspChapter12/WebContent/member.sql
+++ JspChapter12/WebContent/member.sql
@@ -18,3 +18,26 @@ drop table member
 alter table member add regdate date 
 
 insert into member values('aaa','1234','Àü¿µ¹Î','35','male','a@naver.com',sysdate)
+
+create table member
+(
+	id number(30) primary key,
+	email VARCHAR2(255) unique,
+	password varchar2(100),
+	name VARCHAR2(100),
+	regdate date	
+)
+
+create sequence id_num;
+
+insert into member values (id_num.nextval, 'madvirus@madvirus.net', '1234', 'cbk', sysdate);
+
+select sequence_name from user_sequences;
+
+MainForMemberDao.java
+
+Member.java
+
+MainForMemberDao.java
+
+appCtx.xml
